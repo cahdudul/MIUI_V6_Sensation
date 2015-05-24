5061558c597b6d350f4a9cd014c6a750445208de
cahdudul <kaisar.uciha@gmail.com>
2015-05-24 07:56:20 +0700

    tools

---
+++ customize_framework-res.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f $2/drawable-hdpi/default_wallpaper.jpg
+rm -f $2/drawable-nodpi/default_wallpaper.jpg
+rm -f $2/drawable-sw600dp-nodpi/default_wallpaper.jpg
+rm -f $2/drawable-sw720dp-nodpi/default_wallpaper.jpg
+rm -f $2/drawable-xhdpi/default_wallpaper.jpg
+# MIUI_V6_Sensation
