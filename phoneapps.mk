257ef36e693e682e1f2181fefdf9987cbc6292d7
cahdudul <kaisar.uciha@gmail.com>
2015-05-24 08:01:03 +0700

    initial

---
+++ phoneapps.mk
@@ -0,0 +1,40 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := BasicDreams \
+		Bluetooth \
+		BluetoothExt \
+		Camera2 \
+		CellBroadcastReceiver \
+		CertInstaller \
+		DocumentsUI \
+		Galaxy4 \
+		HoloSpiralWallpaper \
+		HTMLViewer \
+		KeyChain \
+		LatinIME \
+		LiveWallpapers \
+		MagicSmokeWallpapers \
+		PacProcessor \
+		PhaseBeam \
+		PhotoPhase \
+		PhotoTable \
+		PrintSpooler \
+		Stk \
+		UserDictionaryProvider \
+		VisualizationWallpapers \
+		WAPPushManager \
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+		DefaultContainerService \
+		DownloadProvider \
+		InputDevices \
+		ExternalStorageProvider \
+		MediaProvider \
+		FusedLocation \
+		ProxyHandler \
+		SettingsProvider \
+		SharedStorageBackup \
+		Shell \
+		WallpaperCropper \
+# MIUI_V6_Sensation
